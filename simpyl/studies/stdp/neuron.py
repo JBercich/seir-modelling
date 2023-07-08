d49dda9368a5c6b13778d6667eea76bd8608eeb3
JBercich <joshbercich@gmail.com>
2023-07-08 09:57:51 +0900

    Added additional neuron documentation and renamed certain attributes

--- simpyl/studies/stdp/neuron.py
+++ simpyl/studies/stdp/neuron.py
@@ -1,36 +1,47 @@
 from enum import StrEnum
 
-NEURON_INITIAL_MEMBRANE_POTENTIAL: float = -70.0
+# Neuron behaviour constants
+NEURON_INITIAL_POTENTIAL: float = -70.0
 NEURON_INITIAL_RECOVERY: float = 0.0
-NEURON_INPUT_CURRENT: float = 10.0
+NEURON_STATIC_CURRENT: float = 10.0
 NEURON_THRESHOLD: float = 30.0
 
 
 class NeuronName(StrEnum):
-    RZ = "Resonator"
+    """
+    Neurons have an identifier name in line with Izhikevich's analysis of different
+    spike behaviours. Each name corresponds to a specific neuron spike condition set.
+    """
+
+    RS = "Regular Spiking"
+    IB = "Intrinsically Bursting"
     CH = "Chattering"
     FS = "Fast Spiking"
-    RS = "Regular Spiking"
-    TC = "Thalamo-Cortical"
-    IB = "Intrinsically Bursting"
     LTS = "Low-Threshold Spiking"
+    TC = "Thalamo-Cortical"
+    RZ = "Resonator"
 
 
 class NeuronType(StrEnum):
-    Other = "Other"
+    """
+    Neurons have a categorical type. These types describe the biological nature of the
+    neuron with respect to how the activity impacts cortical behaviour.
+    """
+
     Excitatory = "Excitatory"
     Inhibitory = "Inhibitory"
+    Other = "Other"
 
 
 class Neuron:
     def __init__(
         self,
-        neuron_name: NeuronName,
-        neuron_type: NeuronType,
+        name: NeuronName,
+        dtype: NeuronType,
         recovery_time_scale: float,
         recovery_sensitivity: float,
-        spike_reset_membrane_potential: float,
-        spike_reset_recovery: float,
+        reset_potential: float,
+        reset_recovery: float,
     ):
         """
         A neuron for the STDP case study is assessed for membrane potential activations
@@ -39,19 +50,19 @@ class Neuron:
         over time.
 
         Args:
-            neuron_name (NeuronName): Name of the neuron.
-            neuron_type (NeuronType): Type of the neuron.
+            name (NeuronName): Name of the neuron.
+            dtype (NeuronType): Type of the neuron.
             recovery_time_scale (float): Time-scale of recovery variable.
             recovery_sensitivity (float): Sensitivity of recovery variable.
-            spike_reset_membrane_potential (float): Spike reset of membrane potential.
-            spike_reset_recovery (float): Spike reset of recovery variable.
+            reset_potential (float): Spike reset of membrane potential.
+            reset_recovery (float): Spike reset of recovery variable.
         """
-        self.name = neuron_name
-        self.type = neuron_type
-        self._recovery_time_scale = recovery_time_scale
-        self._recovery_sensitivity = recovery_sensitivity
-        self._spike_reset_membrane_potential = spike_reset_membrane_potential
-        self._spike_reset_recovery = spike_reset_recovery
+        self.name: NeuronName = name
+        self.dtype: NeuronType = dtype
+        self._recovery_time_scale: float = recovery_time_scale
+        self._recovery_sensitivity: float = recovery_sensitivity
+        self._reset_potential: float = reset_potential
+        self._reset_recovery: float = reset_recovery
 
     def __eq__(self, other) -> bool:
         return self.name == other.name
@@ -59,20 +70,20 @@ class Neuron:
     def __hash__(self) -> int:
         return hash(self.name)
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.name.value
 
-    def get_type(self):
-        return self.type.value
+    def get_dtype(self) -> str:
+        return self.dtype.value
 
-    def get_recovery_time_scale(self):
+    def get_recovery_time_scale(self) -> float:
         return self._recovery_time_scale
 
-    def get_recovery_sensitivity(self):
+    def get_recovery_sensitivity(self) -> float:
         return self._recovery_sensitivity
 
-    def get_spike_reset_membrane_potential(self):
-        return self._spike_reset_membrane_potential
+    def get_reset_potential(self) -> float:
+        return self._reset_potential
 
-    def get_spike_reset_recovery(self):
-        return self._spike_reset_recovery
+    def get_reset_recovery(self) -> float:
+        return self._reset_recovery
--- simpyl/studies/stdp/run.py
+++ simpyl/studies/stdp/run.py
@@ -1,2 +1,5 @@
+# from simpyl.studies.stdp.neuron_simulation import NEURONS
+# from neuron_simulation import NEURONS
+
 if __name__ == "__main__":
     pass
