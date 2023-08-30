a09cf54ce1117137664fadc1aedad699c3933aa8
JBercich <joshbercich@gmail.com>
2023-08-30 13:28:32 +1000

    Cleaned up resource.py

--- simpyl/model/model.py
+++ simpyl/model/model.py
@@ -12,3 +12,30 @@
 #     def __init__(self, elements: list[Element], relationships: list[Relationship]):
 #         self.elements: list[Element] = elements
 #         self.relationships: list[Relationship] = relationships
+
+
+# class FunDecorator:
+#     def __init__(self):
+#         self.registry = []
+
+#     def __call__(self, m):
+#         "This method is called when some method is decorated"
+#         self.registry.append(m)  # Add function/method to the registry
+
+#         def w(my_arg):
+#             print(my_arg, m)
+
+#         return w
+
+
+# run_this_method = FunDecorator()  # Create class instance to be used as decorator
+
+
+# @run_this_method
+# def method_with_custom_name(my_arg):
+#     return "The args is: " + my_arg
+
+
+# # do some magic with each decorated method:
+# for m in run_this_method.registry:
+#     print(m.__name__)
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,4 @@ import pytest
 
 @pytest.fixture
 def abstract_init_error_message():
-    # Error message when instantiating an abstract class
     return "cannot instantiate abstract class"
