7b2d8f98b797d90ba60009be0d80c722e651d3fe
JBercich <joshbercich@gmail.com>
2024-04-06 15:02:09 +1100

    Bump

--- simpyl/model/interactions/interaction.py
+++ simpyl/model/interactions/interaction.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import typing
 from typing import Callable, TypeAlias
 from simpyl.model.resources.resource import Resource
 from simpyl.model.resources.datafield import DataField
@@ -15,3 +16,6 @@ class Interaction:
         self.sources: Sources = sources
         self.targets: Targets = targets
         self.function: InteractionFn = function
+
+    def run(self) -> dict[DataField, typing.Any]:
+        return {t: v for v, t in zip(*self.function(*self.sources), self.targets)}
